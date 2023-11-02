45aff2ffc14ed6c6e7a6264a43262d4b7c8f015f
aabhinaav <aabhinaavgarg@gmail.com>
2023-11-02 14:38:34 +0530

    circuit breaker:- 1. adding dependencies -> actuator for health check, circuitbreaker-resilience4j for circuitbreaker and resilience4j-reactor if using webflux in project
    2. adding configuraitons for health checker by actuator and resilience4j circuitbreaker
    
    3. adding circuit breaker to method

--- order-service/src/main/java/com/learnMS/Order/controller/OrderController.java
+++ order-service/src/main/java/com/learnMS/Order/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.learnMS.Order.controller;
 
 import com.learnMS.Order.dto.OrderRequest;
 import com.learnMS.Order.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,9 +20,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory-service", fallbackMethod = "placeOrder_fallback")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         this.orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String placeOrder_fallback(@RequestBody OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Oops! Something went wrong. Please order after some time!";
     }
 
     @GetMapping
